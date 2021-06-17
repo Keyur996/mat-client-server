2d66d6347459b1c661e4f751a0bc92bfdffe1ccb
Keyur R. Machhi <Y05477@ad.csci.co.in>
2021-06-17 17:00:40 +0530

    server

--- routes/client.routes.js
+++ routes/client.routes.js
@@ -1,21 +1,50 @@
 import express from 'express';
 import Client from "../models/client.model.js";
-
 const clientRoutes = express.Router();
 
-clientRoutes.get('/', (req, res) => {
-    res.json({ message: "Hello World" })
+// get All clients from MongoDB
+clientRoutes.route('/').get((req, res) => {
+    Client.find((err, data) => {
+        if (err) res.status(404).json({ data: null, message: err.message });
+        else res.status(200).json({ data, message: "All Clients" });
+    })
 })
 
 // Add Employee
-clientRoutes.route('/create').post((req, res, next) => {
+clientRoutes.route('/create').post((req, res) => {
     Client.create(req.body, (error, data) => {
         if (error) {
-            return next(error)
+            res.status(404).json({ data: null, message: err.message })
         } else {
             res.status(200).json({ data, message: "Added Successfully" })
         }
     })
 });
+
+clientRoutes.route("/read/:id").get((req, res) => {
+    Client.findById(req.params.id, (err, data) => {
+        if (err) res.status(404).json({ data: null, message: err.message });
+        else res.status(200).json({ data, message: "Client received" });
+    })
+});
+
+clientRoutes.route('/update/:id').put((req, res,) => {
+    Client.findByIdAndUpdate(req.params.id, {
+        $set: req.body
+    }, (error, data) => {
+        if (error) {
+            res.status(404).json({ data: null, message: err.message });
+        } else {
+            res.status(200).json({ data, message: 'Data updated successfully' })
+        }
+    })
+});
+
+clientRoutes.route('/delete/:id').delete((req, res) => {
+    Client.findOneAndRemove(req.params.id, (err, data) => {
+        if (err) res.status(404).json({ message: err.message });
+        else res.status(200).json({ msg: 'Data Deleted' })
+    })
+})
 
 export default clientRoutes;
--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-import bodyParser from "body-parser";
 import clientRoutes from "./routes/client.routes.js";
 import { dbConfig } from "./database/db.js";
-import path from "path";
+// import path from "path";
 
 // Connect To mongoose
 mongoose.Promise = global.Promise;
@@ -14,8 +13,8 @@ mongoose.connect(dbConfig.db, { useNewUrlParser: true, useUnifiedTopology: true 
 
 // Setting up middleware
 const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use("/api", clientRoutes);
 
@@ -24,14 +23,14 @@ const port = process.env.PORT || 5000;
 const server = app.listen(port, () => console.log(`Server is Running on ${port}`));
 
 // Find 404 and hand over to error handler
-app.use((req, res, next) => {
-    next(createError(404));
-});
+// app.use((req, res, next) => {
+//     next(createError(404));
+// });
 
 // error handler
 app.use(function (err, req, res, next) {
     console.error(err.message); // Log error message in our server's console
     if (!err.statusCode) err.statusCode = 500; // If err has no specified error code, set error code to 'Internal Server Error (500)'
-    res.status(err.statusCode).send(err.message); // All HTTP requests must have a response, so let's send back an error with its status code and message
+    res.status(err.statusCode).json({ message: err.message }); // All HTTP requests must have a response, so let's send back an error with its status code and message
 });
 
